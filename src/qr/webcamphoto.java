2e03713d15370d99c1eb22cfd10d0f487d9bf836
Maxence Tallon <maxencetallon@gmail.com>
2019-04-27 19:44:45 +0200

    ajouet webcam_photo

---
+++ src/qr/webcamphoto.java
@@ -0,0 +1,126 @@
+package qr;
+
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JLabel;
+
+import com.github.sarxos.webcam.Webcam;
+
+class  webcamphoto extends Thread {
+	private IDcase executeid;
+		public webcamphoto(String name)  {
+		super(name);
+		executeid = new IDcase();
+		
+		
+	}
+	public void run()    {
+		
+		
+		
+			
+		
+				synchronized(this){try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			
+		}
+		
+		try {
+    		
+    		
+    			while(true){
+    				File file = new File("test.png");
+			ImageIO.write(window.webcam.getImage(),"PNG",file);
+			
+            String decodedText = main.decodeQRCode(file);
+            if(decodedText == null) {
+                
+                window.texte.setText("<html>No QR <br> Code found in the image</html>");
+                
+            }else {
+            	
+                System.out.println("Decoded text = " + decodedText);
+                
+                window.texte.setText(decodedText);
+                window.ID =decodedText.split("<br>")[decodedText.split("<br>").length-1].substring(0,4);
+                window.frameCamera.dispatchEvent(new WindowEvent(window.frameCamera, WindowEvent.WINDOW_CLOSING));
+               executeid.start();
+                synchronized(this){
+                try {
+                	System.out.println("attente de du prochain lancement du QRreader");
+					this.wait();
+					
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}}
+                
+              
+           
+            
+            } }
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	
+		
+		
+	}
+  }
+
+
+
+
+class IDcase implements Runnable {
+	private Thread tid;
+	
+	public void run() {
+		
+		System.out.println("L'ID trouvé est :"+window.ID);
+		
+		switch(window.ID) {
+		
+		case "S001":
+			window.sound_required = 0;
+			break;
+		
+		}
+		synchronized(tid) {
+			
+			try {
+				tid.wait(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		window.ID="undifined";
+		window.texte.setText(null);
+		
+		
+		
+		
+		
+		
+	}
+	public void start() {
+		
+		tid= new Thread(this,"execution de l'id");
+		tid.start();
+	}
+	
+	
+	
+}
+    
+
+
+
